3f42d4789dba983c736efe597147f88efbf5e365
nagato yuki <itarutitor@gmail.com>
2020-11-24 23:32:22 +0000

    Delete light mode, small changes on snow eff

--- source/js/drop.js
+++ source/js/drop.js
@@ -1,10 +1,8 @@
-for (let i = 0; i < Math.floor(window.innerWidth / 64); i++) {
+for (let i = 0; i < Math.floor(window.innerWidth / 16); i++) {
     let tmp = document.createElement("div");
     tmp.classList.add("drop");
     tmp.style.left = Math.floor(Math.random() * 101).toString() + "%";
-    // var co = +Math.floor(Math.random()*16777215).toString(16);
-    // tmp.style.backgroundColor = "#"+co;
     setTimeout(() => {
       document.body.appendChild(tmp);
-    }, Math.floor(Math.random() * 6000));
+    }, Math.floor(Math.random() * 8000));
 }  
